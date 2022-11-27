7b8c8612f4c89b6ae2d1c5e5df508dc178251a25
grochat <grochat@binfservms01.unibe.ch>
2022-11-27 17:30:17 +0100

    Update : step 2 and step 3

--- scripts/add_link_to_data.sh
+++ scripts/add_link_to_data.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-#################
-# This script adds symbolic link to the FASTQ files of the cell subpopulations under analysis
-################
+# path to the project directory
+HOME=/data/courses/rnaseq_course/lncRNAs/Project2/users/grochat
+
+# path to the reference files for the project
+REF=/data/courses/rnaseq_course/lncRNAs/fastq
 
 # add symbolic link to the fastq files of the meroclonal cells in data/ directory
 for i in 2 4 7
 do
-ln -s /data/courses/rnaseq_course/lncRNAs/fastq/3_${i}_L3_R1_*.fastq.gz .
+	ln -s $REF/3_${i}_L3_R1_*.fastq.gz $HOME/data/3_${i}_R1.fastq.gz
+	ln -s $REF/3_${i}_L3_R2_*.fastq.gz $HOME/data/3_${i}_R2.fastq.gz
 done
 
 # add symbolic link to the fastq files of the parental cells in data/ directory
 for i in 1 2 3
 do
-ln -s /data/courses/rnaseq_course/lncRNAs/fastq/P${i}_L3_R1_*.fastq.gz .
+	ln -s $REF/P${i}_L3_R1_*.fastq.gz $HOME/data/P${i}_R1.fastq.gz
+	ln -s $REF/P${i}_L3_R2_*.fastq.gz $HOME/data/P${i}_R2.fastq.gz
 done
