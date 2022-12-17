10c44eb41e624b8bbc54533726773235ab82693b
grochat <grochat@binfservms01.unibe.ch>
2022-12-17 15:29:05 +0100

    Updating step 2 and 3 ; adding step 4

---
+++ scripts/analyse_quantification_transcripts.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# path to the project directory
+HOME=/data/courses/rnaseq_course/lncRNAs/Project2/users/grochat
+
+# for each abundance file given in argument, get the transcripts with non-zero read abundance and add the corresponding gene name
+for file in $@
+do
+	# extract the generic name of the file
+	file_id=$(basename $file | awk -F "_ab" '{print $1}')
+	
+	# extract the transcripts of TPM > 0
+	awk '$5 != "0" { print $0 }' $file > temp.tsv
+	
+	# sort the detected transcripts 
+	# note : use same LOCALE for sort and join for compatibility
+	LANG=en_EN sort -k 1,1 temp.tsv > temp.tsv.sorted
+
+	# add the header for the detected transcripts
+	sed '1i\
+target_id	length	eff_length	est_counts	tpm' temp.tsv.sorted > temp.tsv	
+
+	# add the corresponding gene name and its known status to the detected transcript
+	LANG=en_EN join --header -1 1 -2 1 $HOME/analysis/transcripts_genes_id.tsv.sorted temp.tsv | awk 'BEGIN{OFS="\t"}{print $1,$2,$3,$4,$5,$6,$7}' > ${file_id}_final_abundance.tsv
+
+done 
+
+rm temp.tsv temp.tsv.sorted
---
+++ scripts/step2/align_to_ref_genome.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+module add UHTS/Aligner/hisat/2.2.1
+
+# path to the project directory
+HOME=/data/courses/rnaseq_course/lncRNAs/Project2/users/grochat
+
+# add path of the genome index files to the HISAT2 environment variable 
+HISAT2_INDEXES=/data/courses/rnaseq_course/lncRNAs/Project2/references/
+
+# extract the generic name of the paired files for the output file
+output_file=$(basename $1 | awk -F "_R" '{print $1}')
+
+# save the result of the alignment from STDERR 
+exec 2> $HOME/analysis/results_align_${output_file}.log
+
+# align the paired-end reads to the human genome with HISAT2 
+# option --dta : better compatibility with StringTie
+# option --rna-strandness : first-strand stranded library
+hisat2 -p 8 --rna-strandness RF --dta -x index_genome -1 $1 -2 $2 -S $HOME/data/${output_file}.sam
